ccf258d9147bb7a3902a3b42ce17f71037c95b80
Eiker Yejan <eikeryejan@gmail.com>
2020-06-29 19:54:33 -0400

    Faves tab pagination

--- src/components/tabs/FavesTab.tsx
+++ src/components/tabs/FavesTab.tsx
@@ -1,52 +1,54 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import _ from "lodash"
 import Post from "../posts/Post"
 import Pagination from "../posts/Pagination"
 import Notice from "../Notice"
-import { getStorageItem } from "../../utils"
+import { getPagedFaves } from "../../utils"
 import { PostObject } from "@types"
 
 const FavesTab: React.FC = () => {
   /* State */
   const [posts, setPosts] = useState<PostObject[]>([])
-  const [shownPosts, setShownPosts] = useState(posts.splice(0, 8))
   const [isLoading, setLoading] = useState(true)
   const [currentPage, setCurrentPage] = useState(0)
+
+  /* Posts amountref */
+  const pageLimit = useRef(0)
 
   /* Get posts */
   useEffect(() => {
     // Get from LocalStorage
-    const faves: PostObject[] = JSON.parse(getStorageItem("get_faves") ?? "[]")
+    const [faves, limit] = getPagedFaves(currentPage)
+
+    console.log(limit)
 
     // Update state
     setPosts(faves)
+    pageLimit.current = limit
     setTimeout(() => setLoading(false), 500)
-  }, [])
+  }, [currentPage])
 
   /**
    * Delete post from state
    * @param id
    */
-  const favsCallback = (id: string): void =>
+  const favsCallback = (id: string): void => {
     setPosts((prev) => _.pullAllBy([...prev], [{ id }], "id"))
+  }
 
   /**
    * Update page callback
    * @param value
    */
-  const pageCallback = (value: number): void => {
-    setCurrentPage(value)
-  }
-
-  console.log(posts)
+  const pageCallback = (value: number): void => setCurrentPage(value)
 
   return (
     <div className="columns is-multiline posts-grid">
       {isLoading ? (
         <span className="loader" />
-      ) : shownPosts.length !== 0 ? (
+      ) : posts.length !== 0 ? (
         <>
-          {shownPosts.map((post, i) => (
+          {posts.map((post, i) => (
             <Post
               key={i}
               data={post}
@@ -54,7 +56,11 @@ const FavesTab: React.FC = () => {
               favsCallback={favsCallback}
             />
           ))}
-          <Pagination current={currentPage} callback={pageCallback} />
+          <Pagination
+            current={currentPage}
+            callback={pageCallback}
+            limit={pageLimit.current / 8}
+          />
         </>
       ) : (
         <Notice
