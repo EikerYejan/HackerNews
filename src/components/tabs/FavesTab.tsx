68659cf5bf295c9a7ab4633ca4022071c53d3f5f
Eiker Yejan <eikeryejan@gmail.com>
2020-06-29 13:25:31 -0400

    chore: implement pagination on faves page

--- src/components/tabs/FavesTab.tsx
+++ src/components/tabs/FavesTab.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import _ from "lodash"
 import Post from "../posts/Post"
+import Pagination from "../posts/Pagination"
 import Notice from "../Notice"
 import { getStorageItem } from "../../utils"
 import { PostObject } from "@types"
@@ -8,7 +9,9 @@ import { PostObject } from "@types"
 const FavesTab: React.FC = () => {
   /* State */
   const [posts, setPosts] = useState<PostObject[]>([])
+  const [shownPosts, setShownPosts] = useState(posts.splice(0, 8))
   const [isLoading, setLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(0)
 
   /* Get posts */
   useEffect(() => {
@@ -27,21 +30,32 @@ const FavesTab: React.FC = () => {
   const favsCallback = (id: string): void =>
     setPosts((prev) => _.pullAllBy([...prev], [{ id }], "id"))
 
-  console.table(posts)
+  /**
+   * Update page callback
+   * @param value
+   */
+  const pageCallback = (value: number): void => {
+    setCurrentPage(value)
+  }
+
+  console.log(posts)
 
   return (
     <div className="columns is-multiline posts-grid">
       {isLoading ? (
         <span className="loader" />
-      ) : posts.length !== 0 ? (
-        posts.map((post, i) => (
-          <Post
-            key={i}
-            data={post}
-            isFav={post.isLiked}
-            favsCallback={favsCallback}
-          />
-        ))
+      ) : shownPosts.length !== 0 ? (
+        <>
+          {shownPosts.map((post, i) => (
+            <Post
+              key={i}
+              data={post}
+              isFav={post.isLiked}
+              favsCallback={favsCallback}
+            />
+          ))}
+          <Pagination current={currentPage} callback={pageCallback} />
+        </>
       ) : (
         <Notice
           heading="There's nothing here"
